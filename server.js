9a8262f0926ed8f539d13e61accfcf8e13e8e97f
Michael McBride <michaelmcbride113@gmail.com>
2025-02-14 13:25:53 -0500

    Added buttons, defined new route for new fruits

--- server.js
+++ server.js
@@ -21,6 +21,11 @@ app.get('/', async (requestAnimationFrame, res) => {
     res.render("index.ejs");
 });
 
+app.get('/fruits/new', (req, res) => {
+    res.render("fruits/new.ejs");
+    res.send('this route sends the user a form page!')
+});
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
